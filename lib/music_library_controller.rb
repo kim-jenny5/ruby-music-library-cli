52d60775965bddf2d76f91a6805de8e6343c33d9
Jenny Kim <kimj1905@gmail.com>
2021-04-02 21:27:40 -0400

    commit tree b2db1bd33bb62dab28073d77969ed6d81f967726

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,13 +73,11 @@ class MusicLibraryController
 
     def play_song
         puts "Which song number would you like to play?"
-        # list_songs
         input = gets.strip.to_i
         
         if (1..Song.all.length).include?(input)
             alphabetized = Song.all.sort_by {|song| song.name}
             song = alphabetized[input - 1]
-            # binding.pry
             puts "Playing #{song.name} by #{song.artist.name}"
         end
     end
